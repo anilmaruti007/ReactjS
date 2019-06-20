e59bbbca73bff4d62fbd0e8e04a57f7b6e225126
Anil Maruti <anil.a@BRILLIO.COM>
2019-06-20 18:15:28 +0530

    Initial commit

---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+// function person(){
+//     return(
+//         <h2>Hi, Welcome to react</h2>
+//     )
+// }
+import React from 'react'
+
+const person = () => {
+    return(
+        <h2>Hi, Welcome</h2>
+    )
+}
+
+ export default person;
