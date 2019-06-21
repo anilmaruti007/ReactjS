35d52ed96951e0b0581d85f6c4cb935f4b97b203
Anil Maruti <anil.a@BRILLIO.COM>
2019-06-21 15:52:20 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -25,9 +25,16 @@ class App extends Component {
         <h1>Welcome to React</h1>
         <p>This is working!</p>
         <button onClick={this.switchnameHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Hobbies: Writing Poems</Person>
+          <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+          <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchnameHandler}/>
+          <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>Hobbies: Writing Poems</Person>
       </div>
     )
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -8,7 +8,7 @@ import React from 'react'
 const person = (props) => {
     return(
         <div>
-            <p>I am {props.name} and {props.age} old</p>
+            <p onClick={props.click}>I am {props.name} and {props.age} old</p>
             <p>{props.children}</p>
         </div>
     )
